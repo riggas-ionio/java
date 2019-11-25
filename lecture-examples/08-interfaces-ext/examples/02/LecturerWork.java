2b7eb0247e5572d8254b4f36a9e816493f66860a
Dimitrios Ringas <riggas@ionio.gr>
2019-11-25 21:10:58 +0200

    Revert "Added lecture-6 examples"
    This reverts commit 37aa1881325609c0357ddcbfb4389010dfb3f7b8.

--- lecture-examples/08-interfaces-ext/examples/02/LecturerWork.java
+++
@@ -1,8 +0,0 @@
-public enum LecturerWork { 
-    TEACH ("Be inspiring"), 
-    RESEARCH ("Appear thoughtful");
-    
-    private final String descr;
-    LecturerWork(String d) { this.descr = d; }
-    public String getDescription() { return this.descr; }
-}
--- lecture-examples/08-interfaces-ext/examples/02/Parent.java
+++
@@ -1,6 +0,0 @@
-
-public interface Parent {
-    public void dropOffKids(String where);
-    public void pickUpKids();
-    public void attendParentsMeeting();
-}
