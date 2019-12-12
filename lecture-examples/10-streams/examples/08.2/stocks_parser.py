694b98f817f903a20366556600307bc07100e266
Dimitrios Ringas <riggas@ionio.gr>
2019-12-12 22:30:51 +0200

    Added lecture-10 examples (more: python parser)

--- lecture-examples/10-streams/examples/08.2/stocks_parser.py
+++ lecture-examples/10-streams/examples/08.2/stocks_parser.py
@@ -3,4 +3,5 @@ import json
 with open('stocks.json') as json_file:
     data = json.load(json_file)
     for stock in data:
-        print (stock['name'], '(', stock['symbol'], ') $', stock['currentValue'])
+        #print (stock['name'], '(', stock['symbol'], ') $', stock['currentValue'])
+        print stock['name'], '(', stock['symbol'], ') $', stock['currentValue']
