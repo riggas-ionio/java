402fd63e1284994b6a4e54770226a88df016f208
Dimitrios Ringas <riggas@ionio.gr>
2019-12-12 22:32:48 +0200

    Added lecture-10 examples (improved: CopyBytes)

--- lecture-examples/10-streams/examples/01/CopyBytes.java
+++ lecture-examples/10-streams/examples/01/CopyBytes.java
@@ -12,7 +12,7 @@ public class CopyBytes {
             in = new FileInputStream(args[0]);
             out = new FileOutputStream(args[1]);
             int c; // c is actually byte (8bit)
-            
+
             while ((c = in.read()) != -1) {
                 out.write(c);
             }
@@ -22,7 +22,10 @@ public class CopyBytes {
             System.out.println("Αδυναμία ανάγνωσης αρχείου:"+args[0]);
         } catch (IOException e) {
             System.out.println("Σφάλμα εισόδου-εξόδου.");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Δεν δόθηκαν αρκετές παράμετροι. "+e.getMessage());
         } finally {
+            System.out.println("Cleaning up files streams.");
             if (in != null) {
                 in.close();
             }
