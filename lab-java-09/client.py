831791065775c9829fd7106b84af029f3780f629
Dimitrios Ringas <riggas@ionio.gr>
2019-12-13 10:01:23 +0200

    Added lab-java-10 exercise (improved client.py to accept HOST and PORT as arguments)

--- lab-java-09/client.py
+++ lab-java-09/client.py
@@ -1,8 +1,16 @@
 #Python 3 code
-import socket
+import socket #To get network connection
+import sys #To get argument info
 
-HOST = '192.168.1.5'  # The server's hostname or IP address
-PORT = 1080        # The port used by the server
+if len(sys.argv)<2:
+    print("Usage: python3 'HOST' PORT")
+    sys.exit(1)
+HOST = sys.argv[1]  # The server's hostname or IP address
+PORT = 1080
+if len(sys.argv)<3:
+    print("Using default port: 1080")
+else:
+    PORT = int(sys.argv[2])        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
