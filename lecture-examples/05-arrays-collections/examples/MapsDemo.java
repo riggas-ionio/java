3d702a7a0fe2ac47094de3412375852e3eef5023
Dimitrios Ringas <riggas@gmail.com>
2020-11-24 12:29:47 +0200

    Changed Map demo

--- lecture-examples/05-arrays-collections/examples/MapsDemo.java
+++ lecture-examples/05-arrays-collections/examples/MapsDemo.java
@@ -4,17 +4,22 @@ import java.util.HashMap;
 
 
 public class MapsDemo {
-    
+
     public static void main(String[] args) {
-        Map<String, Integer> m = new HashMap<String, Integer>();
+        Map<String, Integer> m = new TreeMap<String, Integer>();
 
         // Initialize frequency table from command line
         for (String a : args) {
             Integer freq = m.get(a);
-            m.put(a, (freq == null) ? 1 : freq + 1);
+            // m.put(a, (freq == null) ? 1 : freq + 1);
+            System.out.println("Map get key "+a+": "+freq);
+            if ( freq==null )
+                m.put(a, 1); // java promotes int 1 to Integer(1)
+            else
+                m.put(a, freq+1); //java allows Integer object +1
         }
 
-        System.out.println("Count parameters: "+args.length);
+        // System.out.println("Count parameters: "+args.length);
         System.out.println("Distinct parameters: "+m.size());
         System.out.println(m);
     }
