2f637f034f99870d886adb37ea9fee604242309b
Dimitrios Ringas <riggas@gmail.com>
2020-12-01 21:21:28 +0200

    Lecture 07 examples -minor update

--- lecture-examples/07-inheritance/examples/07/Main.java
+++ lecture-examples/07-inheritance/examples/07/Main.java
@@ -6,27 +6,28 @@ public class Main {
         p1.increaseLikes();
         p1.increaseLikes();
         p1.increaseLikes();
-        
+
         TextPost p2 = new TextPost("Goerge", "Here's a thought..");
         p2.increaseLikes();
         p2.increaseLikes();
-        
+
         MyWall wall = new MyWall();
         wall.addPost(p1);
         wall.addPost(p2);
-        
+
         wall.showPosts();
-        
+
         // superclass reference - superclass object: allowed
-        Post p3 = new Post("George"); 
+        Post p3 = new Post("George");
         // subclass reference - subclass object: allowed
-        TextPost p4 = new TextPost("Nancy", "Testing post.."); 
+        TextPost p4 = new TextPost("Nancy", "Testing post..");
         // superclass reference - subclass object:
         // allowed, but can only access superclass members
-        Post p5 = new PhotoPost("George");
+        // Post p5 = new PhotoPost("George");
+        Post p5 = new TextPost("George", "Hre's a thought.."); // allowed, but casting to PhotoPost will fail!
         //p5.setPhoto("photo.jpg"); // COMPILER ERROR
         ((PhotoPost)p5).setPhoto("photo.jpg"); //EXPLICIT CASTING
         // subclass reference - superclass object: not allowed
-        PhotoPost p6 = new Post("Nancy"); // COMPILER ERROR
+        // PhotoPost p6 = new Post("Nancy"); // COMPILER ERROR
     }
 }
