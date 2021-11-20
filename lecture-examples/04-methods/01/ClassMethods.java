614ca07fe8149aac66bd78907a62d67c4a4819e5
Dimitrios Ringas <riggas@ionio.gr>
2021-11-20 23:01:04 +0200

    Added week 04

---
+++ lecture-examples/04-methods/01/ClassMethods.java
@@ -0,0 +1,9 @@
+public class ClassMethods {
+
+    public static void hello(){ System.out.print("Hello "); }
+    public static void main(String[] args){
+      hello();
+      world();
+    }
+    public static void world(){ System.out.println("world!"); }
+}
