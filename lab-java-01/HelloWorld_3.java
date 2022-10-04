1365f6134b857153808c134f84a3016a49c8360a
Dimitrios Ringas <riggas@ionio.gr>
2022-10-04 12:32:37 +0300

    Lab 01 (2022)

---
+++ lab-java-01/HelloWorld_3.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *  Τεκμηρίωση HelloWord προγράμματος
+ *  Αρχείο πηγαίου κώδικα HelloWorld.java
+ * 
+ *  Πρώτο πρόγραμμα σε Java. Εμφανίζει το κείμενο Hello, World στο τερματικό.
+ *  
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ ******************************************************************************/
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.printlr("Hello, World _3");
+    }
