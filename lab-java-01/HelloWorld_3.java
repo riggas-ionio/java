91e560af80211c9479164c611ded10db4b558542
kyprosantreou <kyprosantreou@outlook.com>
2022-10-13 18:05:25 +0300

    new file:   lab-java-01/HelloWorld.class 	modified:   lab-java-01/HelloWorld.java 	new file:   lab-java-01/HelloWorld_1.class 	modified:   lab-java-01/HelloWorld_1.java 	new file:   lab-java-01/HelloWorld_2.class 	modified:   lab-java-01/HelloWorld_2.java 	new file:   lab-java-01/HelloWorld_3.class 	modified:   lab-java-01/HelloWorld_3.java

--- lab-java-01/HelloWorld_3.java
+++ lab-java-01/HelloWorld_3.java
@@ -1,15 +1,7 @@
-/******************************************************************************
- *  Τεκμηρίωση HelloWord προγράμματος
- *  Αρχείο πηγαίου κώδικα HelloWorld.java
- * 
- *  Πρώτο πρόγραμμα σε Java. Εμφανίζει το κείμενο Hello, World στο τερματικό.
- *  
- *  Compilation:  javac HelloWorld.java
- *  Execution:    java HelloWorld
- *
- ******************************************************************************/
+public class HelloWorld_3 {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.printlr("Hello, World _3");
+        System.out.println("Hello, World_3");
     }
+}
