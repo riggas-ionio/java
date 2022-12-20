7c91142bb3806cc75a08e7e80e24f26f850ef57e
kyprosantreou <kyprosantreou@outlook.com>
2022-12-20 19:19:21 +0200

    new file:   Calculator2.class 	new file:   Calculator2.java

---
+++ Calculator2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Calculator2 {
+    public int add(int i1, int i2){
+        int sum=i1+i2;
+        return sum;
+    }
+
+    public int multiply(int i1,int i2){
+        int multi=0;
+        for(int i=0; i<=i1; i++){
+            multi=add(multi,i2);
+        }
+        return multi;
+    }
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        int param1,param2,res;
+
+        System.out.println("Provide two positive intregers: ");
+
+        param1=sc.nextInt();
+        param2=sc.nextInt();
+
+        Calculator2 clt=new Calculator2();
+
+        res=clt.multiply(param1,param2);
+
+        System.out.println(param1 + " times " + param2 + " equals " + res);    
+
+    }
+        
+}
