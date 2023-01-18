98fae89dbc8205d802bfdac0f99f033e280ad682
kyprosantreou <kyprosantreou@outlook.com>
2023-01-18 18:32:44 +0200

    new file:   Arrays3.class 	new file:   Arrays3.java

---
+++ Arrays3.java
@@ -0,0 +1,13 @@
+public class Arrays3 {
+    public static void main(String[] args){
+        int[] nums=new int[10];
+         for(int i=0; i<10; i++){
+            nums[i]=i*i;
+        }
+
+        for(int i=0; i<10; i++){
+            System.out.println(nums[i]);
+        }
+
+    }
+}
