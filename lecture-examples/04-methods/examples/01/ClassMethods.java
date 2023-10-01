63b094e9eeaa6a17d03c276b5ec12d82ed223ba8
Dimitrios Ringas <riggas@ionio.gr>
2023-10-01 12:48:36 +0300

    Cleanup 2022

--- lecture-examples/04-methods/examples/01/ClassMethods.java
+++
@@ -1,9 +0,0 @@
-public class ClassMethods {
-
-    public static void hello(){ System.out.print("Hello "); }
-    public static void main(String[] args){
-      hello();
-      world();
-    }
-    public static void world(){ System.out.println("world!"); }
-}
--- lecture-examples/07-interfaces-abstract/examples-2/02/ProgrammerWork.java
+++
@@ -1,9 +0,0 @@
-public enum ProgrammerWork { 
-    MEET ("Discuss, Design"), 
-    CODE ("Think, Type, Test"), 
-    MAKE_COFFEE ("Make coffee");
-    
-    private final String descr;
-    ProgrammerWork(String d) { this.descr = d; }
-    public String getDescription() { return this.descr; }
-}
