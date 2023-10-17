6be8a258a61514bb1f17a6369cf92b10102f9a65
Dimitrios Ringas <55480871+riggas-ionio@users.noreply.github.com>
2023-10-17 09:46:17 +0300

    Lab 01 missing files

---
+++ lab-java-01/HelloWorld_2.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+ *  Τεκμηρίωση HelloWord προγράμματος
+ *  Αρχείο πηγαίου κώδικα HelloWorld.java
+ * 
+ *  Πρώτο πρόγραμμα σε Java. Εμφανίζει το κείμενο Hello, World στο τερματικό.
+ *  
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ ******************************************************************************/
+
+public class HelloWorld_2 {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.printlr("Hello, World _2");
+    }
+}
