d0369d19299b6fc70b00f0be7eef751f8342da21
Dimitrios Ringas <riggas@ionio.gr>
2023-11-09 21:13:14 +0200

    Lecture examples week 05

---
+++ lecture-examples/04-methods/examples/01/Calculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public int add(int i1, int i2) {
+        int sum = i1 +i2;
+        return sum;
+    }
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int param1, param2;
+        System.out.println("Provide two integers:");
+        param1 = sc.nextInt();
+        param2 = sc.nextInt();
+        
+        Calculator clt = new Calculator();
+        int res = clt.add( param1, param2);
+        System.out.println( param1 +" plus "+ param2 + " equals " +res);
+    }
+}
---
+++ lecture-examples/05-arrays-collections/examples/ArgsLoop.java
@@ -0,0 +1,10 @@
+public class ArgsLoop {
+    
+    public static void main(String[] args){
+
+        for ( int i=0; i<args.length; i++) {
+            System.out.println(args[i]);
+        }
+        
+    }
+}
